e4ebe14391da0dce1d8c7442b4b33e460ab084ee
Felipe Barbosa de Oliveira <felipe.boliveira@outlook.com>
2019-12-05 15:17:52 -0300

    Adiciona exercicio 04

--- modulo02-dom/exercicio04.js
+++ modulo02-dom/exercicio04.js
@@ -2,10 +2,10 @@
 var nomes = ["Diego", "Gabriel", "Lucas"]
 
 // TODO: Verificar utilidade
-var divElement = document.querySelector('body div#divList ul');
+var btnElement = document.getElementById("btn");
 
 // Function para adicionar o elemento na lista
-function addList() {
+function adicionarLista() {
     var containerElement = document.getElementById("list");
 
     // Percorre o array de nomes
@@ -24,6 +24,31 @@ function addList() {
 
 }
 
+function adicionar() {
+    var inputElement = document.getElementById("nome").value;
+    var containerElement = document.getElementById("list");
+
+    if (inputElement == "") {
+        return adicionar();
+    }
+    
+    var listElement = document.createElement("li");
+    var text2 = document.createTextNode(inputElement);
+
+    listElement.appendChild(text2);
+
+    containerElement.appendChild(listElement);
+
+}
+
+function limparInput() {
+    document.getElementById("nome").value = "";
+}
+
 window.addEventListener('load', function (event) {
-    addList();
+    adicionarLista();
+});
+
+btn.addEventListener('click', function (event) {
+    limparInput();
 });

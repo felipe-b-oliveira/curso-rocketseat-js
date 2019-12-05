097770de7c019e5c6c21cd5aadbc7532800cf7b6
Felipe Barbosa de Oliveira <felipe.boliveira@outlook.com>
2019-12-05 15:46:35 -0300

    Adiciona comentarios no exercicio 04

--- modulo02-dom/exercicio04.js
+++ modulo02-dom/exercicio04.js
@@ -1,12 +1,13 @@
 // Inicia arrays de nomes do exercicio
 var nomes = ["Diego", "Gabriel", "Lucas"]
 
-
+// Uso no Event Listener 'click'
 var btnElement = document.getElementById("btn");
 
+// Uso no Event Listener 'keyup'
 var inputEnterElement = document.getElementById("nome");
 
-// Function para adicionar o elemento na lista
+// Function para printar lista na tela
 function adicionarLista() {
     var containerElement = document.getElementById("list");
 
@@ -26,10 +27,13 @@ function adicionarLista() {
 
 }
 
+// Function para adicionar elemento no final da lista
 function adicionar() {
     var inputElement = document.getElementById("nome").value;
     var containerElement = document.getElementById("list");
 
+    /*Se o input estiver vazio, não adiciona o item da lista e retorna 
+    para o início da function*/
     if (inputElement == "") {
         return adicionar();
     }
@@ -43,6 +47,7 @@ function adicionar() {
 
 }
 
+// Function para limpar o input
 function limparInput() {
     document.getElementById("nome").value = "";
 }
@@ -55,6 +60,7 @@ btn.addEventListener('click', function (event) {
     limparInput();
 });
 
+// Listener para adicionar item quando o Enter for pressionado
 inputEnterElement.addEventListener('keyup', function (e) {
     var key = e.which || e.keyCode;
     if (key == 13) {

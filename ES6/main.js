c09c05439766a924140527f58127d6eabed15e34
Felipe Barbosa de Oliveira <felipe.boliveira@outlook.com>
2019-12-10 17:45:38 -0300

    Inicia a apresentacao a Classes

--- ES6/main.js
+++ ES6/main.js
@@ -1,13 +1,31 @@
-class Teste {
-    metodo() {
-
+class List {
+    constructor() {
+        this.data = [];
     }
 
-    outro() {
-
-    }
-
-    proximo() {
-
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
     }
 }
+
+class TodoList extends List {
+    constructor() {
+        // Chama o construtor da classe pai
+        super();
+
+        this.usuario = 'Felipe';
+    }
+
+    mostraUsuario() {
+        console.log(this.usuario);
+    }
+}
+
+var MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function () {
+    MinhaLista.add('Novo todo');
+}
+
+MinhaLista.mostraUsuario();

d7c08168ae46febb8eaf29e085fd4e53ca0f3cb3
felipe-b-oliveira <felipe.boliveira@outlook.com>
2020-01-07 17:26:50 -0300

    Organiza arvore de pastas para as aulas

--- ReactJS/huntweb/src/index.js
+++ ReactJS/huntweb/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Rendereiza o primeiro componente chamado App e jogando o seu conteúdo para a div com id 'root'
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
